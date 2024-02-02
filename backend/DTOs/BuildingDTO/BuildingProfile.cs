05b5a9949530363addd87480318bcda6c7b0ed3d
Nhat Dao <nhatdmhe170198@fpt.edu.vn>
2024-02-02 08:52:45 +0700

    building DTO
    building DTO

---
+++ backend/DTOs/BuildingDTO/BuildingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Backend.Models;
+
+namespace Backend.DTOs.BuildingDTO
+{
+    public class BuildingProfile : Profile
+
+    {
+        public BuildingProfile()
+        {
+            CreateMap<CreateBuildingDTO, Building>()
+            .ForMember(des => des.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<CreateBuildingDTO, Building>()
+            .ForMember(des => des.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<DeleteBuildingDTO, News>();
+        }
+    }
+}
---
+++ backend/DTOs/BuildingDTO/CreateBuildingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.DTOs.BuildingDTO
+{
+    public class CreateBuildingDTO
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
