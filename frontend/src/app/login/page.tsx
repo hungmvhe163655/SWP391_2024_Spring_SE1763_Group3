b3d1d2b524ac55ca717d4a563080610427d085aa
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2024-02-18 16:58:04 +0700

    Some login page

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,17 @@
+import LoginCard from "./loginCard";
+
+const Login = () => {
+  return (
+    <div className="flex flex-1 bg-slate-200">
+      <div className="w-2/3 grid-rows-2 gap-4 place-content-center h-full">
+        <div>He</div>
+        <div>He</div>
+      </div>
+      <div className="w-1/3">
+        <LoginCard />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
