56b86dc2212380b9149fcd2591e755cf3a387442
hungvietmai <hungmvhe163655@fpt.edu.vn>
2024-02-19 23:48:50 +0700

    Update page.tsx

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -4,8 +4,8 @@ const Login = () => {
   return (
     <div className="flex flex-1 bg-slate-200 min-h-screen">
       <div className="w-2/3 grid-rows-2 gap-4 place-content-center">
-        <div>He</div>
-        <div>He</div>
+        <div></div>
+        <div></div>
       </div>
       <div className="w-1/3 m-auto">
         <LoginCard />
