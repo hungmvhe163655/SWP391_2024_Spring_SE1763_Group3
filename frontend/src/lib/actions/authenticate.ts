cbdc55086f8e797e8ced6e70deee1a086c8bff45
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2024-02-25 01:52:42 +0700

    change folder structure

---
+++ frontend/src/lib/actions/authenticate.ts
@@ -0,0 +1,20 @@
+"use sever";
+
+// export const authenticate = async (
+//   _currentState: unknown,
+//   formData: FormData
+// ) => {
+//   try {
+//     await signIn("credentials", formData);
+//   } catch (error) {
+//     if (error) {
+//       switch (error.type) {
+//         case "CredentialsSignin":
+//           return "Invalid credentials.";
+//         default:
+//           return "Something went wrong.";
+//       }
+//     }
+//     throw error;
+//   }
+// };
---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const TENANT_API = process.env.NEXT_PUBLIC_AUTH_API_URL;
