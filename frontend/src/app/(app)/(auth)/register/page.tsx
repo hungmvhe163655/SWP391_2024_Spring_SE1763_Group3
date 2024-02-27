c8842fa3306ef2bd1f74384b62b3c209bcedfba1
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2024-02-27 08:10:09 +0700

    change div tag of register to main

--- frontend/src/app/(app)/(auth)/register/page.tsx
+++ frontend/src/app/(app)/(auth)/register/page.tsx
@@ -2,11 +2,11 @@ import RegisterCard from "./RegisterCard";
 
 const Register = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-slate-200">
+    <main className="flex flex-col min-h-screen bg-slate-200">
       <div className="mx-auto my-10">
         <RegisterCard />
       </div>
-    </div>
+    </main>
   );
 };
 
